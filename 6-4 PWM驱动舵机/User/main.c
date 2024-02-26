ec2843f40b0fbe2947032da98fb09cfca4004c1d
YuanKe <912267428@qq.com>
2024-02-26 23:50:51 +0800

    update

--- 6-4 PWM驱动舵机/User/main.c
+++ 6-4 PWM驱动舵机/User/main.c
@@ -18,13 +18,22 @@ int main(void)
 	
 	while(1)
 	{	
-		KeyNum = Key_GetNum();
-		if (KeyNum == 1)
-		{
-			Angle+=30;
-			if(Angle > 180) Angle = 0;
-		}
-		Servo_SetAngle(Angle);
-		OLED_ShowNum(1,7,Angle,3);
+//		KeyNum = Key_GetNum();
+//		if (KeyNum == 1)
+//		{
+//			Angle+=30;
+//			if(Angle > 180) Angle = 0;
+//		}
+//		Servo_SetAngle(Angle);
+//		OLED_ShowNum(1,7,Angle,3);
+//		while(1)
+//		{
+//			Angle+=30;
+//			if(Angle > 180) Angle = 0;
+//			Servo_SetAngle(Angle);
+//			OLED_ShowNum(1,7,Angle,3);
+//			Delay_ms(500);
+//		}
+		
 	}
 }
